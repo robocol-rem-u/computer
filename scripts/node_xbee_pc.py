cf6c1b4a337afb07fc2ae7b65ba35614690c1fb9
jdserranomu <jd.serranomu@uniandes.edu.co>
2019-06-20 23:33:16 -0500

    Convencion de minuscular en topicos y nodos

--- scripts/node_xbee_pc.py
+++ scripts/node_xbee_pc.py
@@ -10,15 +10,15 @@ def arm_Orders_Callback(parm):
 def traction_Orders_Callback(param):
     pass
 
-def node_XBEE_pc():
+def node_xbee_pc():
 
-    rospy.init_node('node_XBEE_pc', anonymous=True)
+    rospy.init_node('node_xbee_pc', anonymous=True)
     # Se suscribe a al topico de la informacion de la conexion sobre quien manda informacion
-    rospy.Subscriber ('topic_Connection', connection, connection_Callback)
+    rospy.Subscriber ('topic_connection', connection, connection_Callback)
     # Se suscribe a al topico que indica ordenes sobre los brazos
-    rospy.Subscriber ('topic_Arm_Orders', arm_Orders, arm_Orders_Callback)
+    rospy.Subscriber ('topic_arm_orders', arm_Orders, arm_Orders_Callback)
     # Se suscribe a al topico que indica ordenes sobre ruedas
-    rospy.Subscriber ('topic_Traction_Orders', traction_Orders, traction_Orders_Callback)
+    rospy.Subscriber ('topic_traction_orders', traction_Orders, traction_Orders_Callback)
     rate = rospy.Rate (10)
     while not rospy.is_shutdown ():
         rate.sleep ()
@@ -27,6 +27,6 @@ def node_XBEE_pc():
 if __name__ == '__main__':
     try:
 
-        node_XBEE_pc()
+        node_xbee_pc()
     except rospy.ROSInterruptException:
         pass
