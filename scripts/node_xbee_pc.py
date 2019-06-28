b4848ebe9dbea6de25bb5037993dd100fbb0180e
Jorge <jfgf11@gmail.com>
2019-06-27 19:00:01 -0500

    XBEE por ahora no

--- scripts/node_xbee_pc.py
+++ scripts/node_xbee_pc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy, socket, _thread, serial, threading, thread, time
 from master_msgs.msg import connection, arm_Orders, traction_Orders
+import numpy as np
 
 
 #### CONSTANTS ####
@@ -47,7 +48,6 @@ def connection_Callback(param):
     pass
 
 
-
 def arm_Orders_Callback(param):
     pass
 
@@ -59,17 +59,6 @@ def traction_Orders_Callback(param):
 
 
 ### START XBEE CODE ###
-def enviarMensajeInicializacion():
-    global EnviarMensaje
-    while EnviarMensaje:
-        nMsg = 1#3
-        print("Enviando Inicializacion")
-        transmitirMensaje("A"+str(nMsg)+"#I0#I1#I2#I3#I4#I5#")
-        time.sleep(1)
-
-
-
-
 if rf_wifi_selector==1:
     try:
         xBeeSerial = serial.Serial(DIRECCION_XBEE, baudrate=BAUD_RATE)
@@ -90,6 +79,15 @@ if rf_wifi_selector==2:
 if order_connect== 0:
     print("\033[1;31mNo estan activas todas las conexiones, encontrara funcionalidad limitada \033[0;0m")
 print("\t \t -- Modo de control RF --")
+
+### Mensaje de inicializacion ###
+def enviarMensajeInicializacion():
+    global EnviarMensaje
+    while EnviarMensaje:
+        nMsg = 1#3
+        print("Enviando Inicializacion")
+        transmitirMensaje("A"+str(nMsg)+"#I0#I1#I2#I3#I4#I5#")
+        time.sleep(1)
 
 
 #### METODO PARA TRANSMITIR MENSAJES WIFI - RF ####
@@ -122,9 +120,9 @@ def transmitirMensaje(mensaje):
 
 
 
-
 #### CAMBIAR TIPO DE COMUNICACION ####
 def cambiarEstado(nueva_com):
+    global modo
     if nueva_com == 1:
         modo = "C+WIFI"
     elif nueva_com==2:
@@ -142,13 +140,59 @@ def cambiarEstado(nueva_com):
     lock.release()
 
 
+#### METODO PARA RECIBIR LAS ACCIONES DE UN JOYSTICK ####
+# Requiere convertir x, y entre -1 y 1
+# Requiere filtrar las acciones para no hacerlas muy seguido
+def procesarJoystick(x, y, sensibilidad, sobreEscribirPWM=False, PWM_I=0, PWM_D=0):
+    global EnviarMensaje, ultimo_izquierdo, ultimo_derecho
 
+    if not sobreEscribirPWM:
+        (calc_PWM_izq, calc_PWM_der) = steering(x, y, sensibilidad)
+    else:
+        (calc_PWM_izq, calc_PWM_der) = (int(PWM_I), int(PWM_D))
+
+    StringIzquierda = ("L" + str(calc_PWM_izq) + SEPARADOR_NEGATIVO) if (calc_PWM_izq >= 0) else (
+    "L" + str(-calc_PWM_izq) + SEPARADOR_POSITIVO)
+    StringDerecha = ("R" + str(calc_PWM_der) + SEPARADOR_NEGATIVO) if (calc_PWM_der >= 0) else (
+    "R" + str(-calc_PWM_der) + SEPARADOR_POSITIVO)
+
+    MensajeSeguridadMotores = ""
+
+    if np.sign(ultimo_izquierdo) != np.sign(calc_PWM_izq) and calc_PWM_izq != 0 and np.sign(ultimo_izquierdo) != 0:
+        MensajeSeguridadMotores += "L0#"
+
+    if np.sign(ultimo_derecho) != np.sign(calc_PWM_der) and calc_PWM_der != 0 and np.sign(ultimo_derecho) != 0:
+        MensajeSeguridadMotores += "R0#"
+
+    if np.abs(ultimo_izquierdo - calc_PWM_izq) > 3 or np.abs(ultimo_derecho - calc_PWM_der) > 3 or (
+            calc_PWM_der == 0 and ultimo_derecho != 0) or (calc_PWM_izq == 0 and ultimo_izquierdo != 0):
+        EnviarMensaje = not transmitirMensaje(MensajeSeguridadMotores + StringIzquierda + StringDerecha)
+
+        ultimo_izquierdo = calc_PWM_izq
+        ultimo_derecho = calc_PWM_der
+
+#### METODO PARA DETERMINAR LOS PWM A PARTIR DE LAS COORDENADAS DE UN JOYSTICK (Metodo diamante encontrado en internet) ####
+def steering(x, y, sensibilidad_rcv):
+    # convert to polar
+    r = math.hypot(-x, -y)
+    t = math.atan2(-y, -x)
+    # rotate by 45 degrees
+    t += math.pi / 4
+    # back to cartesian
+    left = r * math.cos(t)
+    right = r * math.sin(t)
+    # rescale the new coords
+    left = left * math.sqrt(2)
+    right = right * math.sqrt(2)
+    # clamp to -1/+1
+    left = max(-1, min(left, 1))
+    right = max(-1, min(right, 1))
+    return int(sensibilidad_rcv*left), int(sensibilidad_rcv*right)
 
 
 
 if __name__ == '__main__':
     try:
-
         node_xbee_pc()
     except rospy.ROSInterruptException:
         pass
